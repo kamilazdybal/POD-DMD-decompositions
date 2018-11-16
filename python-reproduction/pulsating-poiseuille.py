f029463f313e0e61b2678541e35f84cdfc8dc9b4
camillejr <kamilazdybal@gmail.com>
2018-11-16 16:22:25 +0100

    Added PCA approximation

--- python-reproduction/pulsating-poiseuille.py
+++ python-reproduction/pulsating-poiseuille.py
@@ -1,6 +1,6 @@
 '''
-Simulation of the 2-D velocity distribution in the pulsating Poiseuille flow in the closed channel.
-
+Simulation of the 2-D velocity distribution in the pulsating Poiseuille flow,
+with the PCA approximation with nPC number of principal components.
 _______________________________
     |\                y ^
     |  \                |
@@ -10,18 +10,18 @@ ____|/_________________________
 
 Data is collected in a matrix of size (Ns x Nt).
 The rows are spatial coordinates and columns are time coordinates.
-
 '''
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+from sklearn.decomposition import PCA
 import math
 
 ## Styles:
 csfont = {'fontname':'Lato', "weight": "light"}
 hfont = {'fontname':'Lato', "weight": "medium"}
-demand_col = '#282850'
-renewables_col = '#f44242'
+solution_col = '#282850'
+approx_col = '#f44242'
 background_col = "#f9f7f2"
 fontLabel = 10
 fontTitle = 12
@@ -29,10 +29,11 @@ fontTitle = 12
 ## Data:
 Wo = 10
 Pa = 60
+nPC = 1 # number of principal components in the approximation
 
 ## Solution:
-Nt = 100
-Ns = 50
+Nt = 200
+Ns = 100
 time = np.linspace(0,50,Nt)
 space = np.linspace(-1,1,Ns)
 u = np.zeros((Ns, Nt))
@@ -45,13 +46,22 @@ for k in range(0, Nt):
 u_mean = np.tile(((1 - space**2)*0.5), [Nt, 1])
 u_R = u + np.transpose(u_mean)
 
+## Perform PCA on a data set:
+pca = PCA(n_components=nPC)
+pca.fit(np.transpose(u_R))
+scores = pca.transform(np.transpose(u_R))
+u_R_PCA = np.transpose(pca.inverse_transform(scores))
+
 ## Simulation:
 fig = plt.figure()
 ax = plt.axes(xlim=(-0.5,1.25), ylim=(-1, 1))
-line, = ax.plot([], [], lw=2)
+line, = ax.plot([], [], lw=2, color=solution_col)
+line_PCA, = ax.plot([], [], '--', lw=2, color=approx_col)
 dashLine = "#cccccc"
 plt.plot([-0.5, 1.25], [0, 0], "--", lw=2, color=dashLine, zorder=-1)
 plt.title("Velocity profile $u(y)$ in the pulsating Poiseuille flow", **csfont, fontsize=fontTitle)
+plt.text(1, 0.85, "Exact", **hfont, fontsize=fontLabel, color=solution_col)
+plt.text(1, 0.75, "First PC", **hfont, fontsize=fontLabel, color=approx_col)
 plt.ylabel("$y$", **csfont, fontsize=fontLabel)
 plt.xlabel("$u(y)$", **csfont, fontsize=fontLabel)
 
@@ -74,12 +84,13 @@ for label in (ax.get_yticklabels()):
 
 def init():
     line.set_data([], [])
-    return line,
+    line_PCA.set_data([], [])
+    return line, line_PCA,
 
 def animate(i):
-    U = u_R[:,i]
-    line.set_data(U, space)
-    return line,
+    line.set_data(u_R[:,i], space)
+    line_PCA.set_data(u_R_PCA[:,i], space)
+    return line, line_PCA,
 
 anim = animation.FuncAnimation(fig, animate, init_func=init, frames=Nt, interval=100, blit=True, repeat=False)
 
